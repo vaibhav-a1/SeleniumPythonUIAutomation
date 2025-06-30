04536cefaaffeb21771d3b06ebd6cfd08f8158f6
vaibhav-a1 <vaibhav.verma@acquia.com>
2025-06-30 15:14:34 +0530

    addition of new file

---
+++ PythonSelenium/src/new/test.py
@@ -0,0 +1,6 @@
+numbers = [25, 78, 13, 56, 97, 33]
+max=numbers[0]
+for num in numbers:
+    if nummax:
+        max=num
+print(max)
---
+++ src/seleniumautomation/modules/custom_attribute/campaign7.py
@@ -0,0 +1,40 @@
+
+from seleniumautomation.constants import LocatorType, ElementFunction, GeneralLocators, CustomAttribute
+from seleniumautomation.error import VegaAppException
+from seleniumautomation.uitest import UiTest
+from seleniumautomation.utils import SeleniumExecutor
+import time
+from selenium.webdriver.common.by import By
+
+##This class is used to test the a360 profiles age.
+class TestCustomAttibute(UiTest, SeleniumExecutor):
+    def campaign_attribute(self):
+        browser = self.login()
+
+        element_type_value = [(LocatorType.XPATH, ElementFunction.CLICK_BUTTON, GeneralLocators.CDP_STUDIO, browser),
+                              (LocatorType.XPATH, ElementFunction.CLICK_BUTTON, CustomAttribute.Launch, browser),
+                              (LocatorType.XPATH, ElementFunction.CLICK_BUTTON, CustomAttribute.TABLE_SELECT, browser),
+                              (LocatorType.XPATH, ElementFunction.CLICK_BUTTON, CustomAttribute.ATTRIBUTE_SELECT, browser)]
+
+
+
+        # Locate the dropdown and select an option
+        dropdown = browser.find_element(By.XPATH, CustomAttribute.TABLE_SELECT)
+        options = dropdown.find_elements(By.TAG_NAME, "option")
+        for option in options:
+            if option.get_attribute("value") == "dispatch":
+                option.click()
+                print("Selected 'campaign' option successfully.")
+            break
+
+        try:
+            for element in element_type_value:
+                self.execute_steps_list([element])
+                time.sleep(5)
+            return browser
+        except Exception as e:
+            raise VegaAppException(f"Exception {e} occurred during a360 steps")
+
+    def test_campaign_attribute(self):
+        browser = None
+        browser = self.campaign_attribute()
